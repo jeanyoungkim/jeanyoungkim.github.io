ac19ebea78307109ef7dfb7aa54817c2d9219018
Jean Kim <jean.kim@onepeloton.com>
2020-03-24 23:20:48 -0400

    generate numbers

--- bingo/main.js
+++ bingo/main.js
@@ -1,14 +1,44 @@
 const board = document.querySelector(".board");
 const numTiles = 25;
+const numColumns = 5;
+const indexArray = [...Array(numTiles).keys()];
+const freeIndex = 12;
 
-const generateBoard = () => {
-  let tiles = "";
-  [...Array(numTiles).keys()].map(i => {
-    const tileMarkup = `<div class="tile"><p data-index="${i}" class="tile-contents"></p></div>`;
-    tiles += tileMarkup;
-    return tiles;
+const columnInfo = [
+  { label: "B", min: 1, max: 15 },
+  { label: "I", min: 16, max: 30 },
+  { label: "N", min: 31, max: 45 },
+  { label: "G", min: 46, max: 60 },
+  { label: "O", min: 61, max: 75 }
+];
+
+// The columns are labeled "B" (numbers 1–15), "I" (numbers 16–30), "N" (numbers 31–45), "G" (numbers 46–60), and "O" (numbers 61–75).[7]
+
+const renderBoard = () => {
+  indexArray.forEach(i => {
+    const tileContent = `<p data-index="${i}" class="tile-contents"></p></div>`;
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    if (i === freeIndex) tile.classList.add("free");
+    tile.innerHTML = tileContent;
+    board.appendChild(tile);
   });
-  board.innerHTML = tiles;
 };
 
-generateBoard();
+renderBoard();
+
+const getRandomNumberBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateNumbers = () => {
+  const tiles = document.querySelectorAll(".tile-contents");
+  indexArray.forEach(i => {
+    const columnNumber = i % 5;
+    const { min, max } = columnInfo[columnNumber];
+    const tileContent =
+      i === freeIndex ? "free" : getRandomNumberBetween(min, max);
+    tiles[i].innerText = tileContent;
+  });
+};
+
+generateNumbers();
