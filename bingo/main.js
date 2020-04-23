8589fb81c8bd927d9e679fcb8305322e604e82f6
Jean Kim <jean.kim@onepeloton.com>
2020-04-23 10:41:37 -0400

    winning combo definitions

--- bingo/main.js
+++ bingo/main.js
@@ -3,6 +3,8 @@ const numTiles = 25;
 const numColumns = 5;
 const indexArray = [...Array(numTiles).keys()];
 const freeIndex = 12;
+let selectedIndices = [];
+let tiles;
 
 const columnInfo = [
   { label: "B", min: 1, max: 15 },
@@ -10,6 +12,21 @@ const columnInfo = [
   { label: "N", min: 31, max: 45 },
   { label: "G", min: 46, max: 60 },
   { label: "O", min: 61, max: 75 },
+];
+
+const winningCombinations = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24],
+  [0, 5, 10, 15, 20],
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24],
+  [0, 6, 12, 18, 24],
+  [4, 8, 12, 16, 20],
 ];
 
 const numNameMap = {
@@ -40,13 +57,29 @@ const renderBoard = () => {
     tile.innerHTML = tileMarkup;
     board.appendChild(tile);
   });
+  tiles = document.querySelectorAll(".tile");
+};
+
+const addEventListeners = () => {
+  tiles.forEach((tile) => {
+    tile.addEventListener("change", (e) => {
+      const changedTile = e.target;
+      const id = parseInt(changedTile.id);
+      if (changedTile.checked) {
+        selectedIndices.push(id);
+      } else {
+        console.log("nuuu remove", selectedIndices, id);
+        selectedIndices = selectedIndices.filter((i) => i !== id);
+      }
+      console.log(selectedIndices);
+    });
+  });
 };
 
 const getRandomNumberBetween = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 const generateNumbers = () => {
-  const tiles = document.querySelectorAll(".tile");
   const tileContents = document.querySelectorAll(".tile-contents");
   const tileInputs = document.querySelectorAll(".tile-input");
 
@@ -68,6 +101,7 @@ const generateNumbers = () => {
 
 const init = () => {
   renderBoard();
+  addEventListeners();
   generateNumbers();
 };
 
